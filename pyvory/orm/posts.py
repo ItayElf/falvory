477a50aa43166bff3f4b732f6e04ec64979a7c06
ItayElf <thh123321@gmail.com>
2022-03-20 08:02:37 +0200

    better post sql

--- pyvory/orm/posts.py
+++ pyvory/orm/posts.py
@@ -3,10 +3,10 @@ from typing import List
 from pyvory.orm import DBConnect
 from pyvory.social import Post
 
-_get_feed = """SELECT u.name, r.id, r.author, r.title, r.description, r.steps, r.cooking_time, r.servings, 
-GROUP_CONCAT(i.name, '~'), GROUP_CONCAT(i.quantity, '~'), GROUP_CONCAT(i.units, '~'), GROUP_CONCAT(u2.name, '~'), 
-GROUP_CONCAT(c.content, '~'), GROUP_CONCAT(c."timestamp", '~'), GROUP_CONCAT(u3.name, '~'), GROUP_CONCAT(u4.name, '~'), 
-p."timestamp", p.id, GROUP_CONCAT(f.followed_id), u.email, u.id
+_get_feed = """
+SELECT u.name, r.id, r.author, r.title, r.description, r.steps, r.cooking_time, r.servings, GROUP_CONCAT(i.name, '~'), 
+GROUP_CONCAT(i.quantity, '~'), GROUP_CONCAT(i.units, '~'), GROUP_CONCAT(u2.name, '~'), GROUP_CONCAT(c.content, '~'), 
+GROUP_CONCAT(c."timestamp", '~'), GROUP_CONCAT(u3.name, '~'), GROUP_CONCAT(u4.name, '~'), p."timestamp", p.id
 FROM posts p
 LEFT JOIN users u ON u.id = p.poster_id
 LEFT JOIN recipes r ON r.id = p.recipe_id
@@ -18,18 +18,19 @@ LEFT JOIN likes l ON l.post_id = p.id
 LEFT JOIN users u3 ON u3.id = l.user_id 
 LEFT JOIN cooked c2 ON c2.post_id = p.id 
 LEFT JOIN users u4 ON u4.id = c2.user_id 
-LEFT JOIN follows f ON f.follower_id = (SELECT id FROM users WHERE email=?)
+WHERE u.id IN (SELECT f2.followed_id FROM follows f2 JOIN users u5 ON u5.id = f2.follower_id WHERE u5.email=?) OR u.email=?
 GROUP BY p.id 
 ORDER BY p."timestamp" DESC
-LIMIT ? OFFSET ?"""
+LIMIT ? OFFSET ?;
+"""
 
 
 def get_feed(email: str, items: int, offset: int) -> List[Post]:
     """Returns a feed of a user"""
     with DBConnect() as c:
-        c.execute(_get_feed, (email, items, offset))
+        c.execute(_get_feed, (email, email, items, offset))
         data = c.fetchall()
         if not data:
             raise Exception("No more posts")
-        posts = [Post.from_tup(tup[:-3]) for tup in data if str(tup[-1]) in tup[-3].split(",") or tup[-2] == email]
+        posts = [Post.from_tup(tup) for tup in data]
         return posts
