c325982058395185d1b77de375752d10d3764d8d
ItayElf <thh123321@gmail.com>
2022-04-01 18:49:48 +0300

    remove unused code

--- pyvory/recipes/ingredients.py
+++ pyvory/recipes/ingredients.py
@@ -1,73 +1,15 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
-from abc import ABC
-from typing import Union
-
-from pyvory.recipes import Volume, Weight
+from dataclasses import dataclass
 
 
 @dataclass
-class Ingredient(ABC):
+class Ingredient:
     """Base class for all ingredients"""
     name: str
     quantity: float
-    units: Union[str, Volume, Weight] = field(init=False)
+    units: str
 
     @staticmethod
     def from_tup(tup) -> Ingredient:
-        name, quantity, units = tup
-        if units in [u.name for u in Volume]:
-            units = Volume[units]
-            return VolumeIngredient(name, quantity, units)
-        elif units in [u.name for u in Weight]:
-            units = Weight[units]
-            return WeightIngredient(name, quantity, units)
-        else:
-            return AbstractIngredient(name, quantity, units)
-
-    @property
-    def units_name(self):
-        return self.units if isinstance(self.units, str) else self.units.name
-
-
-@dataclass
-class VolumeIngredient(Ingredient):
-    """Represents an ingredient that is measured in volume"""
-    units: Volume
-
-    def __post_init__(self):
-        if not isinstance(self.units, Volume):
-            raise ValueError("Non volume units for VolumeIngredient")
-
-    def convert(self, unit: Volume):
-        """Converts the ingredient to a new volume unit"""
-        if not isinstance(unit, Volume):
-            raise ValueError("Non volume units for VolumeIngredient")
-        self.quantity *= self.units.value
-        self.units = unit
-        self.quantity = round(self.quantity / self.units.value, 2)
-
-
-@dataclass
-class WeightIngredient(Ingredient):
-    """Represents an ingredient that is measured in volume"""
-    units: Weight
-
-    def __post_init__(self):
-        if not isinstance(self.units, Weight):
-            raise ValueError("Non weight units for WeightIngredient")
-
-    def convert(self, unit: Weight):
-        """Converts the ingredient to a new volume unit"""
-        if not isinstance(unit, Weight):
-            raise ValueError("Non weight units for WeightIngredient")
-        self.quantity *= self.units.value
-        self.units = unit
-        self.quantity = round(self.quantity / self.units.value, 2)
-
-
-@dataclass
-class AbstractIngredient(Ingredient):
-    """Represents an ingredient that is measured in a unit that is not convertible such as "a pinch" or "2 carrots" """
-    units: str
+        return Ingredient(*tup)
--- pyvory/recipes/recipe.py
+++ pyvory/recipes/recipe.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass, field
 from typing import List, Dict, Optional
 
 from pyvory.recipes import Volume, Weight
-from pyvory.recipes.ingredients import Ingredient, AbstractIngredient, VolumeIngredient, WeightIngredient
+from pyvory.recipes.ingredients import Ingredient
 
 
 @dataclass
@@ -20,29 +20,6 @@ class Recipe:
     cooking_time: Optional[int]  # minutes
     servings: Optional[str]
     idx: Optional[int] = None
-
-    def scale(self, factor: float):
-        """Scales the ingredients and servings by a factor"""
-        for i in range(len(self.ingredients)):
-            self.ingredients[i].quantity *= factor
-        # self.servings *= factor
-        nums = {n: int(n) * factor for n in re.findall(r"\d+?", self.servings)}
-        keys = sorted(nums.keys(), key=lambda x: int(x), reverse=factor > 1)
-        for k in keys:
-            self.servings = self.servings.replace(k, str(nums[k]) if nums[k] % 1 else str(int(nums[k])))
-
-    def convert(self, units_map: Dict[str, str], to_celsius: bool):
-        """Converts the recipe using a units_map and temperature units. Raises KeyError if invalid unit was used."""
-        new_ingredients = []
-        for i in self.ingredients:
-            if not isinstance(i, AbstractIngredient) and i.units.name in units_map:
-                if isinstance(i, VolumeIngredient):
-                    i.convert(Volume[units_map[i.units.name]])
-                elif isinstance(i, WeightIngredient):
-                    i.convert(Weight[units_map[i.units.name]])
-            new_ingredients.append(i)
-        self.ingredients = new_ingredients
-        self.steps = replace_temperature(self.steps, to_celsius)
 
     @classmethod
     def from_tup(cls, tup: tuple) -> Recipe:
